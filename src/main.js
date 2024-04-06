1b8ef7742bd2d1db8e9291d0932c468ada5e661b
Ivan Ivashchenko <ivan86.ivashchenko@gmail.com>
2024-04-06 14:28:34 +0300

    Fix errors eslint

--- src/main.js
+++ src/main.js
@@ -6,57 +6,57 @@ import textResourses from './textResourses.js';
 
 const app = () => {
   const i18nextInstance = i18next.createInstance();
-  i18nextInstance.init(textResourses).then((t) => {
-    // M(state)
-    const state = {
-      isValid: '',
-      errorOrSuccessReport: '',
-      urls: [],
-    };
-    const elements = {
-      form: document.querySelector('.rss-form'),
-      feedback: document.querySelector('.feedback'),
-      urlInput: document.querySelector('#url-input'),
-    };
+  i18nextInstance.init(textResourses);
+  // M(state)
+  const state = {
+    isValid: '',
+    errorOrSuccessReport: '',
+    urls: [],
+  };
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    feedback: document.querySelector('.feedback'),
+    urlInput: document.querySelector('#url-input'),
+  };
 
-    // V(render on-change)
-    const watchedState = onChange(state, (path, value) => {
-      switch (path) {
-        case 'isValid':
-          render(watchedState, elements, value, t);
-          break;
-        default:
-          break;
-      }
+  // V(render on-change)
+  const watchedState = onChange(state, (path, value) => {
+    switch (path) {
+      case 'isValid':
+        render(watchedState, elements, value, i18nextInstance);
+        break;
+      default:
+        break;
+    }
+  });
+
+  // Controller (события ---> state);
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const inputValueUrl = data.get('url');
+
+    const schema = yup.object().shape({
+      inputValueUrl: yup
+        .string()
+        .trim()
+        .required()
+        .url('notValidError')
+        .notOneOf(watchedState.urls, 'alreadyExistsError'),
     });
 
-    // Controller (события ---> state);
-    elements.form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const data = new FormData(e.target);
-      const inputValueUrl = data.get('url');
-
-      const schema = yup.object().shape({
-        inputValueUrl: yup
-          .string()
-          .required()
-          .url('notValidError')
-          .notOneOf(watchedState.urls, 'alreadyExistsError'),
+    schema
+      .validate({ inputValueUrl })
+      .then((value) => {
+        // if here will be 'push' -> onClick will not see changes...
+        watchedState.urls = [...watchedState.urls, value.inputValueUrl];
+        watchedState.errorOrSuccessReport = 'sucсess';
+        watchedState.isValid = true;
+      })
+      .catch((err) => {
+        watchedState.errorOrSuccessReport = err.message;
+        watchedState.isValid = false;
       });
-
-      schema
-        .validate({ inputValueUrl })
-        .then((value) => {
-          // if here will be 'push' -> onClick will not see changes...
-          watchedState.urls = [...watchedState.urls, value.inputValueUrl];
-          watchedState.errorOrSuccessReport = 'sucсess';
-          watchedState.isValid = true;
-        })
-        .catch((err) => {
-          watchedState.errorOrSuccessReport = err.message;
-          watchedState.isValid = false;
-        });
-    });
   });
 };
 
--- src/render.js
+++ src/render.js
@@ -1,16 +1,16 @@
-const render = (watchedState, elements, value, t) => {
-  console.log(watchedState.errorOrSuccessReport);
+const render = (watchedState, elements, value, i18nextInstance) => {
+  const text = i18nextInstance.t(watchedState.errorOrSuccessReport);
   switch (value) {
     case false:
       elements.feedback.classList.add('text-danger');
       elements.feedback.classList.remove('text-success');
-      elements.feedback.textContent = t(watchedState.errorOrSuccessReport);
+      elements.feedback.textContent = text;
       elements.urlInput.classList.add('is-invalid');
       break;
     default:
       elements.feedback.classList.remove('text-danger');
       elements.feedback.classList.add('text-success');
-      elements.feedback.textContent = t(watchedState.errorOrSuccessReport);
+      elements.feedback.textContent = text;
       elements.urlInput.classList.remove('is-invalid');
       elements.form.reset();
       elements.urlInput.focus();
